af42e462d9ad9e09b2b39afeb204da2d873c2450
Felix Canditt <felix.canditt@gmail.com>
2025-07-23 14:49:32 +0200

    added CoreConcept

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import TestComponent from './TestComponent';
-import Header from './Header';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
+import { CORE_CONCEPTS } from './data';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
-
-        <TestComponent />
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept concept={CORE_CONCEPTS[0]} />
+            <CoreConcept concept={CORE_CONCEPTS[1]} />
+            <CoreConcept concept={CORE_CONCEPTS[2]} />
+            <CoreConcept concept={CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+function CoreConcept({ concept }) {
+  return (
+    <li>
+      <img src={concept.image} alt={concept.title}></img>
+      <h3>{concept.title}</h3>
+      <p>{concept.description}</p>
+    </li>
+  );
+}
+
+export default CoreConcept;
